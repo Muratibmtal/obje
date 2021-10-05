c7dfcad55f0662cb0d097fff015d3d4d68722319
Murat <muratibmtal@gmail.com>
2021-10-05 16:10:54 +0300

    dd

--- src/obje/Product.java
+++ src/obje/Product.java
@@ -3,19 +3,20 @@ package obje;
 public class Product {
 	private String name;
 	private double price;
-    private double discoundRate;
-    
-     
-    public double getLastPrice() {
-    	return this.price*(100- this.discoundRate)/100;
-    }
-    public void setDiscoundRate(double discoundRate) {
-        this.discoundRate=discoundRate;
-    }
-    public double getDiscoundRate() {
-    	return this.discoundRate;
-    }
-    
+	private double discoundRate;
+
+	public double getLastPrice() {
+		return this.price * (100 - this.discoundRate) / 100;
+	}
+
+	public void setDiscoundRate(double discoundRate) {
+		this.discoundRate = discoundRate;
+	}
+
+	public double getDiscoundRate() {
+		return this.discoundRate;
+	}
+
 	// discoundRate
 	public void setName(String name) {
 		this.name = name;
